ffaab4c97d248007d45ff0d723e2539e722d9e88
Sirius <sirius.st@outlook.com>
2020-12-02 22:06:53 +0800

    增加sDescription替换成英文，精简代码

--- ACL TOP FAMILY/PYTHON/LogToOneFile.py
+++ ACL TOP FAMILY/PYTHON/LogToOneFile.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import os
-import re
 
 
 def file_filter(filedir, keyword):
@@ -12,40 +11,49 @@ def file_filter(filedir, keyword):
     return targetfiles
 
 
+def replace_desp(desp):
+    for (cn, en) in replace_dic.items():
+        desp = desp.replace(cn, en)
+    return desp
+
+
 def logaddsq(logfullpath, filter_col):
     print(logfullpath)
     tlog0 = pd.read_csv(logfullpath,
                         sep="\t",
                         encoding="utf-16",
                         usecols=filter_col)
+    tlog0["sDescription"] = tlog0["sDescription"].apply(replace_desp)
     tlog1 = pd.read_csv(logfullpath,
                         sep="\t",
                         encoding="utf-16",
                         usecols=filter_col)
+    tlog1["sDescription"] = tlog1["sDescription"].apply(replace_desp)
     tlog1.index = tlog1.index + 1
     logwithsq = pd.merge(tlog1, tlog0, left_index=True, right_index=True)
     return logwithsq
 
 
-#def replace_desp(desp):
-#desp = re.sub(" [RD][A-F][^A-Z]"," <Rack>",desp,count=0,flags=0)
-#desp = re.sub(" [DRS]\d{1,}"," <RackXX>",desp,count=0,flags=0)
-#desp = re.sub("\d{1,}","<XX>",desp,count=0,flags=0)
-
 logpath = r"D:\LogAnalysis\AllWerfenChinaTop\202010\GeneralLogs"
 colfilter = ["sCode", "eType", "dateTime", "funcArea", "sDescription"]
 loglist = file_filter(logpath, ".txt")
+replace_dic = {
+    "开机": "Power up",
+    "初始化": "Initializing",
+    "维护": "Maintenance",
+    "错误": "Error",
+    "紧急停止": "Emergency stop",
+    "忙": "Busy",
+    "诊断": "Diagnostics",
+    "准备": "Ready",
+    "受控停机": "Controlled stop",
+    "未连接": "Not connected",
+    "温度调整": "Adjusting thermal",
+    "分析仪状态从": "Analyzer Status changed from",
+    "变为": "to"
+}
 
-logtemp = logaddsq((logpath + "\\" + loglist[0]), colfilter)
-logtemp = logtemp.dropna(axis=0, how="any")
-logtemp = logtemp[(
-    (logtemp.eType_x == "ERROR") | (logtemp.eType_x == "INFORMATION"))
-                  & ((logtemp.funcArea_x == "Analyzer")
-                     | (logtemp.funcArea_x == "Materials"))]
-logtemp["TopSn"] = loglist[0][:-4]
-logonefile=logtemp
-
-for i in range(1, len(loglist)):
+for i in range(len(loglist)):
     print("Starting load file :{}".format(i))
     logtemp = logaddsq((logpath + "\\" + loglist[i]), colfilter)
     logtemp = logtemp.dropna(axis=0, how="any")
@@ -54,6 +62,9 @@ for i in range(1, len(loglist)):
                       & ((logtemp.funcArea_x == "Analyzer")
                          | (logtemp.funcArea_x == "Materials"))]
     logtemp["TopSn"] = loglist[i][:-4]
+    if i == 0:
+        logonefile = logtemp
+        continue
     logonefile = logonefile.append(logtemp, colfilter)
 
 logonefile = logonefile.rename(
@@ -74,12 +85,6 @@ logonefile["Timediff"] = (
     pd.to_datetime(logonefile["dateTime"])) / pd.Timedelta(1, "S")
 logonefile["Timediff<10s"] = logonefile["Timediff"].apply(lambda x: "Y"
                                                           if x < 10 else "N")
-logonefile["sDescriptionSQ"] = logonefile["sDescriptionSQ"].replace(
-    ".*(忙|Busy).*(Emer|紧急).*",
-    "Analyzer Status changed from Busy to Emergency stop.",
-    regex=True)
-#logonefile["sDescription"] = logonefile["sDescription"].apply(replace_desp)
-
 logonefile.drop(["eTypeSQ", "funcAreaSQ"], axis=1, inplace=True)
 logonefile.insert(0, "TopSn", logonefile.pop("TopSn"))
 logonefile.to_csv((logpath + "\\one.csv"))
