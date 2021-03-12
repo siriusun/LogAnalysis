52784ff6447fc0b14a8a981fc3687aaa75203bde
Sirius <sirius.st@outlook.com>
2021-03-12 09:56:29 +0800

    增加导出其它日志功能

--- ACL TOP FAMILY/TopLogExport.ps1
+++ ACL TOP FAMILY/TopLogExport.ps1
@@ -1,4 +1,5 @@
-#姝よ剼鏈敤浜庢壒閲忔彁鍙朣ite Agent鐢熸垚鐨凾OP鏈�鏂版棩蹇楋紝骞朵互SN鍛戒护|2020-10-26
+#姝よ剼鏈敤浜庢壒閲忔彁鍙朣ite Agent鐢熸垚鐨凾OP鏈�鏂版棩蹇楋紝骞朵互SN鍛戒护|2021-03-12
+#4.0 澧炲姞鍏跺畠鏃ュ織瀵煎嚭
 
 $pth = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location $pth
@@ -36,65 +37,114 @@ function Move-BadLogs {
 
 Write-Host 
 "*********************************
-***  ACL_Top Logs tools V3.0  ***
+***  ACL_Top Logs tools V4.0  ***
 *********************************`n`n"
 
-Write-Host "Starting Generate TopLogs?" -ForegroundColor Yellow -BackgroundColor Black
+Write-Host "Starting to decompress Source TopLogs?" -ForegroundColor Yellow -BackgroundColor Black
 
-$flag = Read-Host "Y/y for Yes, N/n for Quit"
-if ($flag -eq "N" -or $flag -eq "n"){
-    Exit
-}
+$flag = Read-Host "Y/y for Yes, N/n for Next step"
+if ($flag -eq "Y" -or $flag -eq "y"){
 
-$CmprFiles1 = Get-ChildItem -Path ($pth.ToString() + "\DownloadLogs\") | Where-Object { $_.Extension -like ".rar" -or $_.Extension -like ".zip" -or $_.Extension -like ".7z" }
-if ($null -eq $CmprFiles1){
-    "No log files found. Any Key to Exit..."
-    [System.Console]::ReadKey() | Out-Null ; Exit
-}
-
-New-Folder("SourceLogs")
-New-Folder("GeneralLogs")
-New-Folder("BadLogs")
-New-Folder("ProDX")
-
-Get-TopLogs
-
-$TopHash = @{}
-$AllZipTopLogs = Get-ChildItem -Path ($pth + "\SourceLogs\") -Recurse -Filter *DBX*.zip
-
-foreach ($ZipTopLog in $AllZipTopLogs) {
-    $ZipTopLogArray = $ZipTopLog.Name.Split("_")
-    if ($ZipTopLogArray[0] -ne "CHINA"){
-        Move-BadLogs
+    $CmprFiles1 = Get-ChildItem -Path ($pth.ToString() + "\DownloadLogs\") | Where-Object { $_.Extension -like ".rar" -or $_.Extension -like ".zip" -or $_.Extension -like ".7z" }
+    if ($null -eq $CmprFiles1){
+        "No log files found. Any Key to Exit..."
+        [System.Console]::ReadKey() | Out-Null ; Exit
     }
-    elseif ($ZipTopLogArray[3] -ne $ZipTopLogArray[6]) {
-        Move-BadLogs
-    }
-}
 
-$AllZipTopLogs = Get-ChildItem -Path ($pth + "\SourceLogs\") -Recurse -Filter *DBX*.zip
-$i = 0
-foreach ($ZipTopLog in $AllZipTopLogs) {
-    $i ++
-    $TopHash[$ZipTopLog.Name.Split("_")[3]] = $i
-}
-
-$Step2 = 0
-foreach ($Sn in $TopHash.Keys) {
-    "-" * 100
-    $TopLog = (Get-ChildItem -Recurse -Path ($pth + "\SourceLogs\") -Filter ("*" + $Sn + "*DBX*.zip") | Sort-Object -Descending)[0]
-    Write-Host (">>>>: " + ($TopHash.Count - $Step2).ToString() + "  " + $TopLog.FullName) -BackgroundColor Black -ForegroundColor Yellow
-    $NameWithSN = $TopLog.Name.Split("_")[7] + "_" + $Sn + ".txt"
+    New-Folder("SourceLogs")
+    New-Folder("BadLogs")
     
-    $7zComTxt = "-o" + $pth + "\GeneralLogs\"
-    7z.exe e $TopLog.FullName -pfixmeplease $7zComTxt GeneralLog.txt -aos
-    Rename-Item -Path (Get-ChildItem -Path $pth -Recurse -Filter GeneralLog.txt).FullName -NewName $NameWithSN
-    Move-Item -Path $TopLog.FullName -Destination ($pth.ToString() + "\ProDX\")
-    "-" * 100
-    "`n`n"
-    $Step2 ++
+
+    Get-TopLogs
+
+    $TopHash = @{}
+    $AllZipTopLogs = Get-ChildItem -Path ($pth + "\SourceLogs\") -Recurse -Filter *DBX*.zip
+
+    foreach ($ZipTopLog in $AllZipTopLogs) {
+        $ZipTopLogArray = $ZipTopLog.Name.Split("_")
+        if ($ZipTopLogArray[0] -eq "DBX"){
+            $SaNmae = "CHINA_ACLTOP_7X0_" + $ZipTopLog.Name.Split("_")[1] + "_0000028003X_" + $ZipTopLog.Name
+            Rename-Item -Path $ZipTopLog.FullName -NewName $SaNmae
+        }
+        elseif ($ZipTopLogArray[3] -ne $ZipTopLogArray[6]) {
+            Move-BadLogs
+        }
+    }
+
 }
 
+if ($flag -eq "Y" -or $flag -eq "y"){
+    New-Folder("ProDX")
+}
+
+$FormatVar = 0
+While (1){ 
+
+    $TopHash = @{}
+    $AllZipTopLogs = Get-ChildItem -Path ($pth + "\SourceLogs\") -Recurse -Filter *DBX*.zip
+
+    if ($FormatVar -ge 1) {
+        write-host	"`n`n"
+    }
+    $FormatVar ++
+    #选择要输入的Log类型
+    "`n`n"
+    write-host "Select logs to Generate:" -ForegroundColor Yellow -BackgroundColor Black
+    "1 : generalLog`n2 : countersForAllTest`n3 : instrumentStatusStatistics`n4 : Quit"
+    $Select = read-host ">>"
+
+    #定义要查找Log关键字
+    if ($Select -eq 1) {
+        $LogTxt = "generalLog.txt"
+    }
+    elseif ($Select -eq 2) {
+        $LogTxt = "countersForAllTestTypesDeterminations.txt"
+    }
+    elseif ($Select -eq 3) {
+        $LogTxt = "instrumentStatusStatistics.txt"
+    }
+	elseif ($Select -eq 4) {
+        break
+    }
+    else {
+        Write-Host "请正确输入!!!"
+        Continue
+    }
+
+    #创建Log类型哈希表
+    $SelectTable = @{"1" = "generalLog"; "2" = "countersForAllTest"; "3" = "instrumentStatus"}
+    $LogFoldName = $SelectTable[$Select]
+
+    #创建Log输出文件夹
+    New-Folder($LogFoldName)
+
+
+    $AllZipTopLogs = Get-ChildItem -Path ($pth + "\SourceLogs\") -Recurse -Filter *DBX*.zip
+    $i = 0
+    foreach ($ZipTopLog in $AllZipTopLogs) {
+        $i ++
+        $TopHash[$ZipTopLog.Name.Split("_")[3]] = $i
+    }
+
+    $Step2 = 0
+    foreach ($Sn in $TopHash.Keys) {
+        "-" * 100
+        $TopLog = (Get-ChildItem -Recurse -Path ($pth + "\SourceLogs\") -Filter ("*" + $Sn + "*DBX*.zip") | Sort-Object -Descending)[0]
+        Write-Host (">>>>: " + ($TopHash.Count - $Step2).ToString() + "  " + $TopLog.FullName) -BackgroundColor Black -ForegroundColor Yellow
+        $NameWithSN = $TopLog.Name.Split("_")[7] + "_" + $Sn + ".txt"
+    
+        $7zComTxt = "-o" + $pth + "\" + $LogFoldName + "\"
+        7z.exe e $TopLog.FullName -pfixmeplease $7zComTxt $LogTxt -aos
+        Rename-Item -Path (Get-ChildItem -Path $pth -Recurse -Filter $LogTxt).FullName -NewName $NameWithSN
+        if (!(Test-Path ($pth.ToString() + "\ProDX\" + $TopLog.Name))){
+            Copy-Item -Path $TopLog.FullName -Destination ($pth.ToString() + "\ProDX\")
+        }
+        "-" * 100
+        "`n`n"
+        $Step2 ++
+    }
+}
+"`n"
 Write-Host ((Get-ChildItem -Path ($pth.ToString() + "\BadLogs\")).Count.ToString()  + " bad logs" + " "*100) -BackgroundColor Black -ForegroundColor Yellow
 "`n"
 Write-Host "**********  TopLogs Generated  **********" -ForegroundColor Yellow -BackgroundColor Black
