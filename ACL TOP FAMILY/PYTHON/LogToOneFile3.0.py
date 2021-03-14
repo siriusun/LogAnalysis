e408c37267b2dc61327c3839ca703c3a1deb303d
Sirius <sirius.st@outlook.com>
2021-03-15 07:46:32 +0800

    python 3.0,toplog2.0

--- ACL TOP FAMILY/PYTHON/LogToOneFile3.0.py
+++ ACL TOP FAMILY/PYTHON/LogToOneFile3.0.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import copy as cp
 import os
+import datetime as dt
 
-logpath = r"D:\Sync_ColorCloud\LogAnalysis\AllWerfenChinaTop\202103\Data\GeneralLogs"
+start = dt.datetime.now()
+
+logpath = r"D:\Sync_ColorCloud\LogAnalysis\AllWerfenChinaTop\202102\Data\GeneralLogs"
 peroid = 0 #日志保留天数,0则全部保留
 colfilter = ["sCode", "eType", "dateTime", "funcArea", "sDescription"]
 replace_dic = {
@@ -17,34 +21,27 @@ replace_dic = {
     "未连接": "Not connected",
     "温度调整": "Adjusting thermal",
     "分析仪状态从": "Analyzer Status changed from",
-    "变为": "to",
-    "样品": "SAMPLE",
-    "试剂1": "REAGENT 1",
-    "试剂2": "REAGENT 2"
+    "变为": "to"#,
+    #"样品": "SAMPLE",
+    #"试剂1": "REAGENT 1",
+    #"试剂2": "REAGENT 2"
 }
 
 Filter_List_sDescription = [
-    "Analyzer Status changed from Ready to Maintenance.",
-    "Analyzer Status changed from Maintenance to Ready.",
-    "Analyzer Status changed from Ready to Busy.",
-    "Analyzer Status changed from Busy to Ready.",
-    "Analyzer Status changed from Not connected to Power up.",
-    "Analyzer Status changed from Power up to Initializing.",
-    "Analyzer Status changed from Initializing to Adjusting thermal.",
-    "Analyzer Status changed from Adjusting thermal to Ready.",
-    "Analyzer Status changed from Ready to Initializing.",
-    "Analyzer Status changed from Busy to Controlled stop.",
-    "Analyzer Status changed from Controlled stop to Busy.",
-    "Analyzer Status changed from Emergency stop to Not connected.",
-    "Analyzer Status changed from Error to Ready.",
-    "Analyzer Status changed from Ready to Error.",
-    "Analyzer Status changed from Diagnostics to Emergency stop.",
-    "Analyzer Status changed from Power up to Diagnostics."
+    "INFORMATION:Analyzer Status changed from Busy to Controlled stop.",
+    "INFORMATION:Analyzer Status changed from Busy to Emergency stop.",
+    "INFORMATION:Analyzer Status changed from Controlled stop to Emergency stop.",
+    "INFORMATION:Analyzer Status changed from Initializing to Emergency stop.",
+    "INFORMATION:Analyzer Status changed from Initializing to Error.",
+    "INFORMATION:Analyzer Status changed from Maintenance to Emergency stop.",
+    "INFORMATION:Analyzer Status changed from Maintenance to Error.",
 ]
 
 Filter_List_sCode = [
     "'03218", "'03004", "'02083", "'02025", "'03215", "'03188", "'02055"
 ]
+
+Filter_List_funcArea = ["Analyzer", "Materials"]
 
 def file_filter(filedir, keyword):
     allfilelist = os.listdir(filedir)
@@ -54,10 +51,16 @@ def file_filter(filedir, keyword):
             targetfiles.append(f)
     return targetfiles
 
+def info_filter(infostr):
+    if ("ERROR:" in infostr) or (infostr in Filter_List_sDescription):
+        return "Y"
+    else:
+        return "N"
 
 def replace_desp(desp):
-    for (cn, en) in replace_dic.items():
-        desp = desp.replace(cn, en)
+    if "分析仪状态从" in desp:
+        for (cn, en) in replace_dic.items():
+            desp = desp.replace(cn, en)
     return desp
 
 
@@ -69,34 +72,29 @@ def logaddsq(logfullpath, filter_col,
                         encoding="utf-16",
                         usecols=filter_col)
     tlog0 = tlog0.dropna(axis=0, how="any")
+    log_gen_time = pd.to_datetime(tlog0.iloc[-1, 2])
+    #筛选掉无用数据
+    tlog0 = tlog0[tlog0.funcArea.isin(Filter_List_funcArea)]
     tlog0["sDescription"] = tlog0["sDescription"].apply(replace_desp)
-    tlog1 = pd.read_csv(logfullpath,
-                        sep="\t",
-                        encoding="utf-16",
-                        usecols=filter_col)
-    tlog1 = tlog1.dropna(axis=0, how="any")
-    tlog1["sDescription"] = tlog1["sDescription"].apply(replace_desp)
+    tlog0["Info_Desp"] = (tlog0["eType"] + ":" + tlog0["sDescription"])
+    tlog0["Info_Desp"] = tlog0["Info_Desp"].apply(info_filter)
+    tlog0 = tlog0[tlog0.Info_Desp == "Y"]
+    tlog0.pop("Info_Desp")
+    tlog0 = tlog0[~tlog0.sCode.isin(Filter_List_sCode)]
+    tlog0.reset_index(drop=True,inplace=True)
+    tlog1 = cp.copy(tlog0)
     tlog1.index = tlog1.index + 1
     logwithsq = pd.merge(tlog1, tlog0, left_index=True, right_index=True)
-    log_gen_time = pd.to_datetime(logwithsq.iloc[-1, 2])
     if log_days != 0:
         logwithsq["log_days"] = (log_gen_time - pd.to_datetime(logwithsq["dateTime_x"])
                 ) / pd.Timedelta(1, "d") - log_days < 0
         logwithsq = logwithsq[logwithsq.log_days == True]
     return logwithsq
 
-
 loglist = file_filter(logpath, ".txt")
 for i in range(len(loglist)):
-    print("Starting load file :{}".format(i))
+    print("Starting load file :{}".format(i+1))
     logtemp = logaddsq((logpath + "\\" + loglist[i]), colfilter,peroid) #最后参数为0，则保留全部日志，填写数字，保留固定天数日志。
-    logtemp = logtemp[(
-        (logtemp.eType_x == "ERROR") | (logtemp.eType_x == "INFORMATION"))
-                      & ((logtemp.funcArea_x == "Analyzer")
-                         | (logtemp.funcArea_x == "Materials"))]
-    #筛选掉无用数据
-    logtemp = logtemp[~logtemp.sDescription_x.isin(Filter_List_sDescription)]
-    logtemp = logtemp[~logtemp.sCode_x.isin(Filter_List_sCode)]
     logtemp["TopSn"] = loglist[i][:-4]
     if i == 0:
         logonefile = logtemp
@@ -126,3 +124,4 @@ logonefile.insert(0, "TopSn", logonefile.pop("TopSn"))
 logonefile["log_days"] = peroid
 logonefile.to_csv((logpath + "\\one.csv"))
 print("Done")
+print(dt.datetime.now() - start)
