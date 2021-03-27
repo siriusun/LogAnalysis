91b25eaa23ed7a6a9348ba8c2acd45d985314d87
Sirius <sirius.st@outlook.com>
2021-03-27 14:31:12 +0800

    增加scode筛选代码；输出Index列名、编号UTF8；EN 注释

--- ACL TOP FAMILY/PYTHON/LogToOneFile3.0.py
+++ ACL TOP FAMILY/PYTHON/LogToOneFile3.0.py
@@ -11,7 +11,7 @@ print(logpath)
 print("*" * 150)
 os.system("pause")
 start = dt.datetime.now()
-peroid = 0 #日志保留天数,0则全部保留
+peroid = 0  #log reserve days; 0 means all.
 colfilter = ["sCode", "eType", "dateTime", "funcArea", "sDescription"]
 replace_dic = {
     "开机": "Power up",
@@ -32,20 +32,58 @@ replace_dic = {
     "试剂2": "REAGENT 2"
 }
 
+#"Analyzer Status changed from Busy to Controlled stop.",
+#Selected sDescription list
 Filter_List_sDescription = [
-    "INFORMATION:Analyzer Status changed from Busy to Controlled stop.",
-    "INFORMATION:Analyzer Status changed from Busy to Emergency stop.",
-    "INFORMATION:Analyzer Status changed from Controlled stop to Emergency stop.",
-    "INFORMATION:Analyzer Status changed from Initializing to Emergency stop.",
-    "INFORMATION:Analyzer Status changed from Initializing to Error.",
-    "INFORMATION:Analyzer Status changed from Maintenance to Emergency stop.",
-    "INFORMATION:Analyzer Status changed from Maintenance to Error.",
+    "Analyzer Status changed from Busy to Emergency stop.",
+    "Analyzer Status changed from Controlled stop to Emergency stop.",
+    "Analyzer Status changed from Initializing to Emergency stop.",
+    "Analyzer Status changed from Initializing to Error.",
+    "Analyzer Status changed from Maintenance to Emergency stop.",
+    "Analyzer Status changed from Maintenance to Error."
 ]
 
+#Unselected sCode list
 Filter_List_sCode = [
-    "'03218", "'03004", "'02083", "'02025", "'03215", "'03188", "'02055"
+    "'03218", "'03004", "'02083", "'02025", "'03215", "'03188", "'02055",
+    "'03184", "'01285", "'01336", "'02077", "'03007", "'03014", "'00056",
+    "'03013", "'03084", "'03016", "'03085", "'03211", "'03020", "'03019",
+    "'03005", "'03011", "'03018", "'03015", "'03017", "'03009", "'03010",
+    "'03021"
 ]
+'''
+03218: Non-identified Sample in rack <rack> position <position>, track <track>.
+03004: Non-identified Material in rack <rack> position <position>, track <track>.
+02083: <Material> insufficient material to run additional calibration / test.
+02025: Cuvettes waste drawer missing.
+03215: LAS sample expiration time exceeded for <sample>.
+03188: Volume check not performed (Factor Diluent in rack <rack>, position <position>, <track>).
+02055: Insufficient volume in position <position>, track <track>.
+03184: <test> Patient job not feasible. Sample <sample> has expired.
+01285: Probe <ARM> Liquid Level Detection error in rack position <position>, track <track>.
+01336: Probe LAS Liquid Level Detection error in LAS Arm cover aspiration point.
+02077: Insufficient volume in LAS Arm cover aspiration point.
+03007: Unknown lot (<Material> in rack <rack> position <position>, track <track>).
+03014: Placement error (<Material> in rack <rack>, position <position>, track <track>).
+00056: Interrupted communication between the Analyzer and the Control Module.
+03013: Placement error (<Material> in rack <rack>, position <position>, track <track>).
+03084: <Material> on-board stability expired for last available vial.
+03016: Placement error (<Material> in rack <rack>, position <position>, track <track>).
+03085: Anti Chromogenic Sub expired.
+03211: Auto Run failed.
+03020: Placement error (<Material> in rack <rack>, position <position>, track <track>).
+03019: Placement error (<Material> in rack <rack>, position <position>, track <track>).
+03005: Unknown material in rack <rack> position <position>, track <track>.
+03011: Placement error
+03018: Placement error
+03015: Placement error
+03017: Placement error
+03009: Placement error
+03021: Placement error
+03010: Placement error
+'''
 
+#Selected funcArea/eType list
 Filter_List_funcArea = ["Analyzer", "Materials"]
 Filter_List_eType = ["ERROR", "INFORMATION"]
 
@@ -58,11 +96,13 @@ def file_filter(filedir, keyword):
             targetfiles.append(f)
     return targetfiles
 
+
 def info_filter(infostr):
     if ("ERROR:" in infostr) or (infostr in Filter_List_sDescription):
         return "Y"
     else:
         return "N"
+
 
 def replace_desp(desp):
     if "分析仪状态从" != desp[0:6]:
@@ -71,8 +111,9 @@ def replace_desp(desp):
         desp = desp.replace(cn, en)
     return desp
 
+
 def logaddsq(logfullpath, filter_col,
-             log_days):  #log_days为0，则保留全部日志，传入数字，保留固定天数日志。
+             log_days):  #log_days: log reserve days; 0 means all.
     print(logfullpath)
     tlog0 = pd.read_csv(logfullpath,
                         sep="\t",
@@ -88,20 +129,22 @@ def logaddsq(logfullpath, filter_col,
                   | ((tlog0.eType == "INFORMATION")
                      & (tlog0.sDescription.isin(Filter_List_sDescription)))]
     tlog0 = tlog0[~tlog0.sCode.isin(Filter_List_sCode)]
-    tlog0.reset_index(drop=True,inplace=True)
+    tlog0.reset_index(drop=True, inplace=True)
     tlog1 = cp.copy(tlog0)
     tlog1.index = tlog1.index + 1
     logwithsq = pd.merge(tlog1, tlog0, left_index=True, right_index=True)
     if log_days != 0:
-        logwithsq["log_days"] = (log_gen_time - pd.to_datetime(logwithsq["dateTime_x"])
-                ) / pd.Timedelta(1, "d") - log_days < 0
+        logwithsq["log_days"] = (log_gen_time - pd.to_datetime(
+            logwithsq["dateTime_x"])) / pd.Timedelta(1, "d") - log_days < 0
         logwithsq = logwithsq[logwithsq.log_days == True]
     return logwithsq
 
+
 loglist = file_filter(logpath, ".txt")
 for i in range(len(loglist)):
-    print("Starting load file :{}".format(i+1))
-    logtemp = logaddsq((logpath + "\\" + loglist[i]), colfilter,peroid) #最后参数为0，则保留全部日志，填写数字，保留固定天数日志。
+    print("Starting load file :{}".format(i + 1))
+    logtemp = logaddsq((logpath + "\\" + loglist[i]), colfilter,
+                       peroid)  #peroid: log reserve days; 0 means all.
     logtemp["TopSn"] = loglist[i][:-4]
     if i == 0:
         logonefile = logtemp
@@ -129,7 +172,9 @@ logonefile["Timediff<10s"] = logonefile["Timediff"].apply(lambda x: "Y"
 logonefile.drop(["eTypeSQ", "funcAreaSQ", "dateTimeSQ"], axis=1, inplace=True)
 logonefile.insert(0, "TopSn", logonefile.pop("TopSn"))
 logonefile["log_days"] = peroid
-logonefile.to_csv((logpath + "\\one.csv"),index_label="Index",encoding="utf_8")
+logonefile.to_csv((logpath + "\\one.csv"),
+                  index_label="Index",
+                  encoding="utf_8")
 print("Done")
 
 print(dt.datetime.now() - start)
