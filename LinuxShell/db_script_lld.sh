ed924c2d76b4b86a79c13148cfb7d0f3d24a2f7e
Sirius <sirius.st@outlook.com>
2021-04-16 20:52:38 +0800

    na

--- LinuxShell/db_script_lld.sh
+++ LinuxShell/db_script_lld.sh
@@ -20,18 +20,26 @@ lld_filter(){
 	fi
 }
 
-echo -e '\nSelect Function to Filter Trace File(1/2):'
-echo -e "\n1 : ErrorLLD(1427) in Clean Cup\n2 : ErrorLLD(1419) in Special Location\n3 : ErrorLLD(1427) in Special Location\n>>:\c"
-read select_func
-if [ "$select_func" -eq 1 ] ; then 
-    grep -E "\|Reagent Arm\|Clean Step - Start\|.*\|7\|0\|0\||\|Reagent Arm\|LLD est\. ms\||\|Reagent Arm\|AspLldCheck\|" append.csv | grep -A 2 -E "\|Clean Step - Start\|" > LldCleanCupR1.csv
-	grep -E "\|Reagent Arm\|Clean Step - Start\|.*\|7\|0\|0\||\|GenLog\|.*\|1427\|REAGENT 1\|" append.csv | grep -B 1 -E "\|GenLog\|" > ER1427LldCleanCupR1.csv
-    grep -E "\|Start Arm\|Clean Step - Start\|.*\|7\|0\|0\||\|Start Arm\|LLD est\. ms\||\|Start Arm\|AspLldCheck\|" append.csv | grep -A 2 -E "\|Clean Step - Start\|" > LldCleanCupR2.csv
-	grep -E "\|Start Arm\|Clean Step - Start\|.*\|7\|0\|0\||\|GenLog\|.*\|1427\|REAGENT 2\|" append.csv | grep -B 1 -E "\|GenLog\|" > ER1427LldCleanCupR2.csv
-elif [ "$select_func" -eq 2 ] ; then
-	get_location
-	lld_filter 1419
-elif [ "$select_func" -eq 3 ] ; then
-	get_location
-	lld_filter 1425
-fi
+while true; do
+	echo -e "\nSelect Function to Filter Trace File(1/2):"
+	echo -e "\n1 : ErrorLLD(1427) in Clean Cup\n2 : ErrorLLD(1419) in Special Location\n3 : ErrorLLD(1427) in Special Location\n4 : exit\n>>:\c"
+	read select_func
+	if [ "$select_func" -eq 1 ] ; then 
+		grep -E "\|Reagent Arm\|Clean Step - Start\|.*\|7\|0\|0\||\|Reagent Arm\|LLD est\. ms\||\|Reagent Arm\|AspLldCheck\|" append.csv | grep -A 2 -E "\|Clean Step - Start\|" > LldCleanCupR1.csv
+		grep -E "\|Reagent Arm\|Clean Step - Start\|.*\|7\|0\|0\||\|GenLog\|.*\|1427\|REAGENT 1\|" append.csv | grep -B 1 -E "\|GenLog\|" > ER1427LldCleanCupR1.csv
+		grep -E "\|Start Arm\|Clean Step - Start\|.*\|7\|0\|0\||\|Start Arm\|LLD est\. ms\||\|Start Arm\|AspLldCheck\|" append.csv | grep -A 2 -E "\|Clean Step - Start\|" > LldCleanCupR2.csv
+		grep -E "\|Start Arm\|Clean Step - Start\|.*\|7\|0\|0\||\|GenLog\|.*\|1427\|REAGENT 2\|" append.csv | grep -B 1 -E "\|GenLog\|" > ER1427LldCleanCupR2.csv
+	elif [ "$select_func" -eq 2 ] ; then
+		get_location
+		lld_filter 1419
+	elif [ "$select_func" -eq 3 ] ; then
+		get_location
+		lld_filter 1425
+	elif [ "$select_func" -eq 4 ] ; then
+		exit
+	else
+		echo -e "\nPlease Input 1-4 Next Run!!"
+	fi
+done
+
+
