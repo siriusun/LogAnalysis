9c21bb62067a167f1a19c7ee423a9fffd7a4156f
Sirius <sirius.st@outlook.com>
2021-06-30 08:41:02 +0800

    删除无用文件，重命令

--- ACL TOP FAMILY/RenameGeneralLogs.ps1
+++
@@ -1,9 +0,0 @@
-$Pth = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$GeneralLogs = Get-ChildItem -Path ($Pth + "\GeneralLogs\") -Filter *.txt
-
-foreach ($GeneralLog in $GeneralLogs){
-	$GeneralLog.FullName.ToString()
-    Rename-Item -Path $GeneralLog.FullName ("T" + $GeneralLog.Name.Split("_")[1])
-}
-"Any Key to Exit..."
-[System.Console]::ReadKey() | Out-Null ; Exit
