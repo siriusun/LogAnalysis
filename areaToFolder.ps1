0400d479fec9b456d80f590dd690db900c17c81a
Sirius <sirius.st@outlook.com>
2021-07-29 21:50:02 +0800

    exam script

---
+++ areaToFolder.ps1
@@ -0,0 +1,35 @@
+$work_pth = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Set-Location $work_pth
+function New-Folder {
+    param (
+        $NewFolderName
+    )
+    if (Test-Path $NewFolderName) {
+        Remove-Item -Recurse $NewFolderName
+    }
+    New-Item -Path $work_pth -ItemType Directory -Name $NewFolderName
+}
+
+$fses = Import-Csv "ServiceFSElist.csv"
+$fseDic = @{}
+
+foreach ($_ in $fses) {
+    $fseDic[$_.NameCN] = $_.Area
+}
+
+$area = "凝血维修基本知识月考2021.xx-北区", "凝血维修基本知识月考2021.xx-南区", "凝血维修基本知识月考2021.xx-西区", "凝血维修基本知识月考2021.xx-东1区", "凝血维修基本知识月考2021.xx-东2区"
+foreach ($_ in $area) {
+    New-Folder $_
+}
+
+$papers = Get-ChildItem -Path $work_pth -Recurse -Filter *.docx
+foreach ($paper in $papers){
+    $fse = $fseDic[$paper.Name.Split("】")[0].Split("【")[1]]
+    switch ($fse) {
+        "North" { Move-Item -Path $paper.Fullname -Destination ($work_pth + "\凝血维修基本知识月考2021.xx-北区") }
+        "South" { Move-Item -Path $paper.Fullname -Destination ($work_pth + "\凝血维修基本知识月考2021.xx-南区") }
+        "West" { Move-Item -Path $paper.Fullname -Destination ($work_pth + "\凝血维修基本知识月考2021.xx-西区") }
+        "East1" { Move-Item -Path $paper.Fullname -Destination ($work_pth + "\凝血维修基本知识月考2021.xx-东1区") }
+        "East2" { Move-Item -Path $paper.Fullname -Destination ($work_pth + "\凝血维修基本知识月考2021.xx-东2区") }
+    }
+}
