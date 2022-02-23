11f98729b8df999b1a3ef32d237a67910a620479
Sirius <sirius.st@outlook.com>
2022-02-23 13:47:59 +0800

    readlines -> read

--- ACL TOP FAMILY/PYTHON/texts_to_one.py
+++ ACL TOP FAMILY/PYTHON/texts_to_one.py
@@ -14,16 +14,14 @@ start = datetime.datetime.now()
 logfs = os.scandir(text_folder_path)
 if os.path.exists(text_folder_path + "\one.txt"):
     os.remove(text_folder_path + "\one.txt")
-one = open(text_folder_path + "\one.txt","a",encoding="utf_8")
-i = 1
-for logf in logfs:
-    if ".log" in logf.name:
-        print(f"{i}: {logf.name}")
-        with open(logf,"r",encoding="utf_16_le") as f:
-            one.write("".join(f.readlines()))
-            f.close()
-        i += 1
-one.close()
+with open(text_folder_path + "\one.txt","a",encoding="utf_8") as one:
+    i = 1
+    for logf in logfs:
+        if ".log" in logf.name:
+            print(f"{i}: {logf.name}")
+            with open(logf,"r",encoding="utf_16_le") as f:
+                one.write(f.read())
+            i += 1
 
 print("\n",datetime.datetime.now() -start,"\n")
 
