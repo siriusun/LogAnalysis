5762969bf509cba67cf35f89ba986e037b106bde
siriusun <sirius.st@outlook.com>
2023-11-19 19:49:44 +0800

    调整错误命名的提示词颜色

--- TCA/extractTCAlogDCS.ps1
+++ TCA/extractTCAlogDCS.ps1
@@ -43,8 +43,9 @@ foreach ($TcaLog in $TcaLogs){
     if ($TcaLog.Name -notmatch "^TCA_H\d\d\d_\w+_\w{2,3}_2(3|4|5)(0[1-9]|1[0-2])(0[1-9]|1\d|2[0-9]|3[01])\.(zip|ZIP|rar|RAR|7z|7Z)$"){
         $logname = $TcaLog.Name
         "`n"
-        Write-Host "Please using standard format to name the TCA log: $logname" -ForegroundColor Yellow
-        Write-Host "Standard format: TCA_流水线短号_医院中文标准名称_姓名_六位日期.rar/zip/7z" -ForegroundColor Red
+        Write-Host "Please using standard format to name the TCA log:" -ForegroundColor Yellow
+        Write-Host $logname -ForegroundColor Red
+        Write-Host "Standard format: TCA_流水线短号_医院中文标准名称_姓名_六位日期.rar/zip/7z" -ForegroundColor Yellow
         Write-Host "Any key to exit..." -ForegroundColor Yellow
         [System.Console]::ReadKey() | Out-Null ; Exit
     }
