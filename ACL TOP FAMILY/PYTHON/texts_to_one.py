96a5c5eb382db40cab7736bf4a66af8594a1a37d
siriusun <sirius.st@outlook.com>
2024-10-24 22:12:48 +0800

    no

--- ACL TOP FAMILY/PYTHON/texts_to_one.py
+++ ACL TOP FAMILY/PYTHON/texts_to_one.py
@@ -1,11 +1,3 @@
-'''
-Author: siriusun sirius.st@outlook.com
-Date: 2023-03-17 07:05:29
-LastEditors: siriusun sirius.st@outlook.com
-LastEditTime: 2024-05-23 07:03:37
-FilePath: \undefinedc:\Users\siriu\Documents\GitHub\LogAnalysis\ACL TOP FAMILY\PYTHON\texts_to_one.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 import os
 import datetime
 import tkinter.filedialog as tk
@@ -20,9 +12,9 @@ os.system("pause")
 start = datetime.datetime.now()
 
 logfs = os.scandir(text_folder_path)
-if os.path.exists(text_folder_path + r"\one.txt"):
-    os.remove(text_folder_path + r"\one.txt")
-with open(text_folder_path + r"\one.txt", "a", encoding="utf_8") as one:
+if os.path.exists(text_folder_path + "\\one.txt"):
+    os.remove(text_folder_path + "\\one.txt")
+with open(text_folder_path + "\\one.txt", "a", encoding="utf_8") as one:
     i = 1
     for logf in logfs:
         if ".log" in logf.name:
